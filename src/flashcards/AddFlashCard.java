3ff4e8aa6d3ba8a073bc420c79497bfbcc4ec345
CA17 <44103268+FiveMinHero@users.noreply.github.com>
2019-04-01 14:09:03 -0400

    Add card with writer

--- src/flashcards/AddFlashCard.java
+++ src/flashcards/AddFlashCard.java
@@ -4,16 +4,25 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AddFlashCard extends JFrame implements ActionListener{
     private JButton submit = new JButton("Submit");
+    private JButton close = new JButton("Close");  
     private JTextField question = new JTextField();
     private JTextField answer = new JTextField();
     private JLabel questLabel = new JLabel("Please write a question for the flash card");
     private JLabel ansLabel = new JLabel("Please write an answer for the flash card");
-    private GridLayout grid1 = new GridLayout(5, 1, 20, 20);
-    private String[] quest = new String[10];
-    private String[] ans = new String[10];
+    private GridLayout grid1 = new GridLayout(6, 1, 20, 20);
+    private static String[] quest = new String[10];
+    private static String[] ans = new String[10];
     private int x = 0;
     
     public AddFlashCard(){
@@ -24,18 +33,64 @@ public class AddFlashCard extends JFrame implements ActionListener{
         add(ansLabel);
         add(answer);
         add(submit);
-        submit.addActionListener(this);
+        add(close);
+        submit.addActionListener(this);    
+        close.addActionListener(this);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        
+        if(source == submit)
+        {
         quest[x] = question.getText();
         ans[x] = answer.getText();
         System.out.println(quest[x] + "\n" + ans[x] + "\n");
         question.setText("");
-        answer.setText("");
+        answer.setText("");  
         x++;
+        }
+        
+        else if(source == close)
+        {
+            
+        
+        try
+        {
+        Path outFiles = Paths.get("C:\\Java\\FlashCardsOut.txt");
+        OutputStream output = new
+                    BufferedOutputStream(Files.newOutputStream(outFiles));
+            BufferedWriter writer = new
+                    BufferedWriter(new OutputStreamWriter(output));
+            String s = " ";
+            for(int c = 0; c < 3; c++)
+            {
+                s = quest[c];
+                if(s != null)
+                {
+                writer.write(s, 0, s.length());
+                writer.write(System.getProperty( "line.separator" ));
+                }
+                s = ans[c];
+                if(s != null)
+                {
+                writer.write(s, 0, s.length());
+                writer.write(System.getProperty( "line.separator" ));
+                }
+            }
+            writer.close();
+            System.exit(0);
+        }
+        catch(IOException ex) {
+            
+            System.out.println(ex);
+            
+        }
+        }
+    
     }
     public static void main(String[] args) {
+
         AddFlashCard frame = new AddFlashCard();
         frame.setSize(300, 300);
         frame.setVisible(true);
