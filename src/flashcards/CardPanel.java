a1d16525c86b9138d5bdb829894c55db6701da8b
Joey Pekar <47425421+JoeyPekar@users.noreply.github.com>
2019-04-01 09:27:29 -0400

    Implementing CardPanel.java class

---
+++ src/flashcards/CardPanel.java
@@ -0,0 +1,57 @@
+/*
+ * CardPanel.java
+ * 
+ * Developed by Joey Pekar on 3/28/2019
+ * Contains code for a panel with a two sided system.
+ */
+
+package flashcards;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CardPanel extends JPanel {
+    
+    private JLabel lblDisplay = new JLabel();
+    
+    private String cardFront;
+    private String cardBack;
+    
+    public CardPanel(String frontText, String backText) {
+        
+        // Variables
+        this.cardFront = frontText;
+        this.cardBack = backText;
+        
+        Font cardFont = new Font("Consolas", Font.BOLD, 24);
+        
+        // Card Design     
+        this.setLayout(new BorderLayout());
+        
+        this.add(lblDisplay, BorderLayout.CENTER);
+        
+        lblDisplay.setText(cardFront);
+        
+        // Change background and styling
+        setBackground(Color.WHITE);
+        lblDisplay.setHorizontalAlignment(SwingConstants.CENTER);
+        lblDisplay.setFont(cardFont);
+        
+    }
+    
+    // "Flips" the panel
+    public void flip() {
+        
+        if (lblDisplay.getText().equals(cardFront)) {
+            
+            lblDisplay.setText(cardBack);
+            
+        } else {
+            
+            lblDisplay.setText(cardFront);
+            
+        }
+        
+    }
+    
+}
