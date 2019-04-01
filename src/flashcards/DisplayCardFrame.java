2b695b0f1ad31acd690aa46eeb99ae1f12a1fa41
Joey Pekar <47425421+JoeyPekar@users.noreply.github.com>
2019-04-01 09:37:22 -0400

    Adding in File Code

--- src/flashcards/DisplayCardFrame.java
+++ src/flashcards/DisplayCardFrame.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.nio.file.*;
+import java.io.*;
+
 public abstract class DisplayCardFrame extends JFrame implements ActionListener {
     
     // Menu System
@@ -54,6 +57,54 @@ public abstract class DisplayCardFrame extends JFrame implements ActionListener 
     private void loadWords() {
         
         this.cardPanel = new CardPanel("Hello", "World");
+        
+        Path inFiles = Paths.get("C:\\Java\\FlashCardsIn.txt");
+        Path outFiles = Paths.get("C:\\Java\\FlashCardsOut.txt");
+        
+        String[] questionArray = new String[10];
+        String[] answerArray = new String[10];
+        int questionCount = 0;
+        int answerCount = 0;
+        int count = 0;
+        String s; 
+        
+        try
+        {
+            InputStream input = Files.newInputStream(inFiles);
+            BufferedReader reader = new 
+                BufferedReader(new InputStreamReader(input));
+            OutputStream output = new
+                    BufferedOutputStream(Files.newOutputStream(outFiles));
+            BufferedWriter writer = new
+                    BufferedWriter(new OutputStreamWriter(output));
+            s = reader.readLine();
+            
+            while(s != null)
+            {
+                if(count % 2 == 1)
+                {
+                    questionArray[questionCount] = s;
+                    writer.write(s, 0, s.length());
+                    writer.write(System.getProperty( "line.separator" ));
+                    s = reader.readLine();
+                    questionCount++;
+                }
+                else
+                {
+                    answerArray[answerCount] = s;
+                    writer.write(s + "\n", 0, s.length());
+                    writer.write(System.getProperty( "line.separator" ));
+                    s = reader.readLine();
+                    answerCount++;
+                }
+                count++;
+            }
+        writer.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
         
     }
     
